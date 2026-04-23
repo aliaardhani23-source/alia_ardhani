a3f965ac7a6eb44dc49235d7fcc94d81b903b0c5
aliaardhani23-source <aliaardhani23@gmail.com>
2026-04-23 09:37:25 +0700

    first commit

---
+++ app/Models/Product.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+     protected $fillable = [
+        'user_id',
+        'nama_produk',
+        'stok',
+        'harga',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+}
